d44fbbec4701f23397314dc192505e6cbf8a54ca
Cody <cydoentis@gmail.com>
2025-10-13 20:42:25 -0400

    refactor: social icons now have hover states and cursor-pointers

--- src/features/socials/components/SocialIcon.tsx
+++ src/features/socials/components/SocialIcon.tsx
@@ -13,7 +13,7 @@ type SocialIconProps = {
 
 function SocialIcon({ item }: SocialIconProps) {
   return (
-    <Link to={item.to} className="px-2 py-2 rounded-sm bg-primary/10 border border-primary/20 text-foreground">
+    <Link to={item.to} className="px-2 py-2 rounded-sm bg-primary/10 border border-primary/20 text-foreground hover:bg-primary/20 cursor-pointer">
       {item.icon}
     </Link>
   );
