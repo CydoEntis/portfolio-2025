eeb93fb4abe8eab32dd71ef1fa0032d61c7b2b55
Cody <cydoentis@gmail.com>
2025-10-13 20:43:08 -0400

    refactor: socials now have proper links

--- src/features/socials/data/socials.tsx
+++ src/features/socials/data/socials.tsx
@@ -3,17 +3,17 @@ import { Github, Linkedin, Twitter } from "lucide-react";
 export const socials = [
   {
     name: "Github",
-    to: "#",
+    to: "https://github.com/CydoEntis",
     icon: <Github size={16} />,
   },
   {
     name: "Linked In",
-    to: "#",
+    to: "https://www.linkedin.com/in/cody-stine-dev/",
     icon: <Linkedin size={16} />,
   },
   {
     name: "Twitter",
-    to: "#",
+    to: "https://x.com/CydoEntis",
     icon: <Twitter size={16} />,
   },
 ];
