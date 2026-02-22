93375bc441ac77006803011d51cc6c316724536b
Cydo Entis <cydoentis@gmail.com>
2026-02-22 13:20:41 -0500

    chore: remove old about, socials, and legacy project components

--- src/features/hero/components/HeroImage.tsx
+++
@@ -1,9 +0,0 @@
-type HeroImageProps = {
-  img: string;
-};
-
-function HeroImage({ img }: HeroImageProps) {
-  return <img src={img} alt="Cody Stine" className="w-full h-full object-cover" />;
-}
-
-export default HeroImage;
